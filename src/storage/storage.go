74735b72bf13f5f8e9deb89a0b057115f23fb13e
CameronBadman <cbadwork@gmail.com>
2025-10-09 02:50:50 +1000

    added fixed storage bug

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -32,26 +32,33 @@ func (fs *FileStorage) Save(t *types.Tree) error {
 		}
 	}
 
-	for dim := 0; dim < 512; dim++ {
-		if err := binary.Write(f, binary.LittleEndian, int32(len(t.Index[dim]))); err != nil {
-			return err
-		}
-		for _, idx := range t.Index[dim] {
-			if err := binary.Write(f, binary.LittleEndian, idx); err != nil {
-				return err
-			}
-		}
-	}
-
 	return nil
 }
 
 func (fs *FileStorage) Load() (*types.Tree, error) {
 	f, err := os.Open(fs.path)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return &types.Tree{
+				Nodes: []types.Node{},
+				Index: [512][]int32{},
+			}, nil
+		}
 		return nil, err
 	}
 	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if info.Size() == 0 {
+		return &types.Tree{
+			Nodes: []types.Node{},
+			Index: [512][]int32{},
+		}, nil
+	}
 
 	var nodeCount int64
 	if err := binary.Read(f, binary.LittleEndian, &nodeCount); err != nil {
@@ -69,18 +76,7 @@ func (fs *FileStorage) Load() (*types.Tree, error) {
 		}
 	}
 
-	for dim := 0; dim < 512; dim++ {
-		var indexLen int32
-		if err := binary.Read(f, binary.LittleEndian, &indexLen); err != nil {
-			return nil, err
-		}
-		t.Index[dim] = make([]int32, indexLen)
-		for i := range t.Index[dim] {
-			if err := binary.Read(f, binary.LittleEndian, &t.Index[dim][i]); err != nil {
-				return nil, err
-			}
-		}
-	}
+	t.RebuildIndex()
 
 	return t, nil
 }
@@ -94,6 +90,7 @@ func writeNode(w io.Writer, n *types.Node) error {
 	if err := binary.Write(w, binary.LittleEndian, int64(len(valueBytes))); err != nil {
 		return err
 	}
+
 	_, err := w.Write(valueBytes)
 	return err
 }
@@ -112,7 +109,7 @@ func readNode(r io.Reader, n *types.Node) error {
 	if _, err := io.ReadFull(r, valueBytes); err != nil {
 		return err
 	}
-	n.Value = string(valueBytes)
 
+	n.Value = string(valueBytes)
 	return nil
 }
