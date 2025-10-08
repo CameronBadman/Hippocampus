3eeba5f468c3e95333aa55ec3635ad73505d5cb7
CameronBadman <cbadwork@gmail.com>
2025-10-09 04:21:30 +1000

    fixed rebuild issue on new tree

--- src/types/types.go
+++ src/types/types.go
@@ -21,21 +21,30 @@ func NewTree() *Tree {
 
 func (t *Tree) Insert(key [512]float32, value string) {
 	nodeIdx := int32(len(t.Nodes))
-	
 	node := Node{
 		Key:   key,
 		Value: value,
 	}
 	t.Nodes = append(t.Nodes, node)
-	
 	for dim := 0; dim < 512; dim++ {
 		insertPos := sort.Search(len(t.Index[dim]), func(i int) bool {
 			return t.Nodes[t.Index[dim][i]].Key[dim] >= key[dim]
 		})
-		
 		t.Index[dim] = append(t.Index[dim], 0)
 		copy(t.Index[dim][insertPos+1:], t.Index[dim][insertPos:])
 		t.Index[dim][insertPos] = nodeIdx
+	}
+}
+
+func (t *Tree) RebuildIndex() {
+	for dim := 0; dim < 512; dim++ {
+		t.Index[dim] = make([]int32, len(t.Nodes))
+		for i := range t.Nodes {
+			t.Index[dim][i] = int32(i)
+		}
+		sort.Slice(t.Index[dim], func(i, j int) bool {
+			return t.Nodes[t.Index[dim][i]].Key[dim] < t.Nodes[t.Index[dim][j]].Key[dim]
+		})
 	}
 }
 
@@ -43,33 +52,26 @@ func (t *Tree) Search(query [512]float32, epsilon float32) []Node {
 	if len(t.Nodes) == 0 {
 		return nil
 	}
-	
 	candidateSet := make(map[int32]int)
-	
 	for dim := 0; dim < 512; dim++ {
 		minVal := query[dim] - epsilon
 		maxVal := query[dim] + epsilon
-		
 		startIdx := sort.Search(len(t.Index[dim]), func(i int) bool {
 			return t.Nodes[t.Index[dim][i]].Key[dim] >= minVal
 		})
-		
 		endIdx := sort.Search(len(t.Index[dim]), func(i int) bool {
 			return t.Nodes[t.Index[dim][i]].Key[dim] > maxVal
 		})
-		
 		for i := startIdx; i < endIdx; i++ {
 			nodeIdx := t.Index[dim][i]
 			candidateSet[nodeIdx]++
 		}
 	}
-	
 	results := make([]Node, 0)
 	for nodeIdx, count := range candidateSet {
 		if count == 512 {
 			results = append(results, t.Nodes[nodeIdx])
 		}
 	}
-	
 	return results
 }
